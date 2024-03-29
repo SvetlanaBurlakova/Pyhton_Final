4464fe55f86f866add5c4648515481501d7db00e
Svetlana Burlakova <svetlya4ok83@gmail.com>
2024-03-29 23:56:23 +0400

    Fix syntax

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
-urlpatterns += re_path(r"^media/(?P<path>.*)$", serve, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT})]
